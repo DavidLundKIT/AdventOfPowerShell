0020326340ad681d9e38412ed02708b1b437ee24
David Lund <david.lund@knowit.se>
2022-12-06 19:31:04 +0100

    Day 1 Advent of code in Powershell

---
+++ AoC2022/Day01inPowershell.ps1
@@ -0,0 +1,8 @@
+﻿# Powershell Advent of code 2022 Day 1 
+param([string] $filepath)
+
+Get-Content $filepath | ForEach-Object -Begin { $sum = 0 } -Process { if ($_ -eq '' ) { Write-Output $sum; $sum = 0 } else {$sum = $sum + $_ } } | Sort-Object -Descending | Select-Object -First 1 
+
+
+Get-Content $filepath | ForEach-Object -Begin { $sum = 0 } -Process { if ($_ -eq '' ) { Write-Output $sum; $sum = 0 } else { $sum = $sum + $_ } } | Sort-Object -Descending | Select-Object -First 3 | ForEach-Object -Begin { $sum = 0 } -Process {$sum = $sum + $_ } -End { Write-Output $sum }
+
